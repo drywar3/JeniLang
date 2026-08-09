16579210bc3c8eefa1aa16cd650f6aa85a78dab6
drywar3 <dry.software.code@gmail.com>
2026-08-09 20:09:16 +0100

    (general) initial commit

--- src/parser/parse_statement.cpp
+++ src/parser/parse_statement.cpp
@@ -2,7 +2,10 @@
 #include "lexer/token.hpp"
 #include "parser/parse_impl.hpp"
 #include "parser/parser.hpp"
+#include <algorithm>
+#include <cassert>
 #include <initializer_list>
+#include <optional>
 
 static constexpr std::initializer_list<TokenKind> STMT_HEADS = {
     TokenKind::KwVar,
@@ -19,12 +22,24 @@ static constexpr std::initializer_list<TokenKind> STMT_HEADS = {
     TokenKind::KwPanic,
 };
 
+auto parse_compound_statement(Parser &parser)
+    -> std::optional<StatementRef> {
+    assert(parser.previous().kind == TokenKind::SpOpenBrace);
+    while (!parser.is(TokenKind::SpCloseBrace)) {
+
+    }
+    parser.try_eat(TokenKind::SpCloseBrace);
+    return std::nullopt;
+}
+
 auto parse_statement(Parser &parser) -> std::optional<StatementRef> {
     if (auto head = parser.try_eat_one_of(STMT_HEADS); head.has_value()) {
         switch (head.value().kind) {
             case TokenKind::KwVar:
             case TokenKind::KwConst:
                 return parse_variable_declaration(parser, {});
+            case TokenKind::SpOpenBrace:
+                return parse_compound_statement(parser);
             default:
                 TODO();
         }
